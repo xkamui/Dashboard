0ae5dcec80cb81a30c1d72356183e853d778b35c
Kamui Shirou <x_kamui@msn.com>
2022-04-03 03:39:35 +0200

    Fixing position of Searchbar and Recentlogs

--- src/Counter/Chart.js
+++ src/Counter/Chart.js
@@ -1,24 +1,21 @@
 import ChartBar from "./ChartBar";
-import RecentLogs from "./RecentLogs";
-import SearchBar from "./SearchBar";
 
 export default function Chart() {
+  const chartValues = [
+    { count: 33, key: "AS", height: 10.216718266254 },
+    { count: 61, key: "US", height: 18.885448916409 },
+    { count: 60, key: "SW", height: 18.575851393189 },
+    { count: 24, key: "UD", height: 7.4303405572755 },
+    { count: 145, key: "AD", height: 44.891640866873 },
+  ];
 
-    const chartValues = [
-        { count: 33, key: "AS", height: 10.216718266254 },
-        { count: 61, key: "US", height: 18.885448916409 },
-        { count: 60, key: "SW", height: 18.575851393189 },
-        { count: 24, key: "UD", height: 7.4303405572755 },
-        { count: 145, key: "AD", height: 44.891640866873 }
-    ]
-
-    return (
+  return (
     <div className="chart">
       <div className="datas">
-        {chartValues.map((bar) => <ChartBar props={bar} key={bar.key} />)}
+        {chartValues.map((bar) => (
+          <ChartBar props={bar} key={bar.key} />
+        ))}
       </div>
-      <SearchBar />
-      <RecentLogs />
     </div>
   );
 }
--- src/Counter/index.js
+++ src/Counter/index.js
@@ -1,13 +1,17 @@
 import "../assets/styles/Counter.css";
 import Chart from "./Chart";
 import Header from "./Header";
+import RecentLogs from "./RecentLogs";
+import SearchBar from "./SearchBar";
 
 export default function Counter() {
   return (
     <div id="count" className="board">
       <Header />
       <div className="wrapper">
-          <Chart />
+        <Chart />
+        <SearchBar />
+        <RecentLogs />
       </div>
     </div>
   );
