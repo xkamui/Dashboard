473aa2738866f49f3c4d5648b04343a3922b8010
Kamui Shirou <x_kamui@msn.com>
2022-04-03 02:37:37 +0200

    HTML header for the counter component

--- src/Counter/index.js
+++ src/Counter/index.js
@@ -1,13 +1,23 @@
-import '../assets/styles/Counter.css';
+import "../assets/styles/Counter.css";
 
-export default function Counter(){
-
-    return (
-        <div id="count" className="board">
-            <div className="wrapper">
-                Counter
-            </div>
+export default function Counter() {
+  return (
+    <div id="count" className="board">
+      <div className="board-header">
+        <div className="current">323</div>
+        <div className="title">
+          <h3>
+            COUNTER
+            <span>
+              Total: <span className="total-users">3071</span>
+            </span>
+          </h3>
         </div>
-    );
-
+        <div className="reset">
+          <button className="reset-counter">Reset</button>
+        </div>
+      </div>
+      <div className="wrapper">Counter</div>
+    </div>
+  );
 }
