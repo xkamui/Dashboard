4405a5ceb7a8eb21bada53495069b4914b20323a
Kamui Shirou <x_kamui@msn.com>
2022-04-09 14:19:38 +0200

    Initial 'Memos' commit

---
+++ src/components/Memos/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className="board-header">
+      Header
+    </div>
+  );
+}
---
+++ src/components/Memos/index.js
@@ -0,0 +1,12 @@
+import "../../assets/styles/Memos.css";
+import React from "react";
+// import axios from "axios";
+import Header from "./Header";
+
+export default function Memos() {
+  return (
+    <div id="notes" className="board">
+      <Header />
+    </div>
+  );
+}
