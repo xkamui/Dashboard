1abca7d157681a403bdb45483a8c58c65042fc2c
Kamui Shirou <x_kamui@msn.com>
2022-04-10 14:28:32 +0200

    Revert "Memos admin reasons"

---
+++ src/components/Notes/index.js
@@ -0,0 +1,9 @@
+import "../../assets/styles/Notes.css";
+
+export default function Notes() {
+  return (
+    <div id="notes" className="board">
+      <div className="wrapper">Notes</div>
+    </div>
+  );
+}
